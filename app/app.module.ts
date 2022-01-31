82d841fd2bc89a991d80d4b6f53fb2e7722d3283
devindran <devindran@gmail.com>
2022-01-31 14:24:13 +0800

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core';
 import { HelloFrameworkModule } from './hello-framework';
-import { MyChildModule} from './my-child';
+import { MyChildModule } from './my-child.module';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [
-    CoreModule,
-    HelloFrameworkModule,
-    MyChildModule
-  ],
+  imports: [CoreModule, HelloFrameworkModule, MyChildModule],
 })
 export class AppModule {}
